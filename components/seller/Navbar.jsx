22984035ecb40223f8445e5777deca7f77196e49
Usif-200 <yousefbeats808@gmail.com>
2025-05-30 06:04:21 +0300

    first commit

---
+++ components/seller/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import Image from 'next/image'
+import { useAppContext } from '@/context/AppContext'
+
+const Navbar = () => {
+
+  const { router } = useAppContext()
+
+  return (
+    <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
+      <h1 className="text-2xl cursor-pointer"><span className="text-orange-600">COCO</span>MART</h1>
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
